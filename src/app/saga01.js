81abf70435a26545486e3ee9cd93c1c00eba0e55
Ashish Dutta <ashish.dutta050@gmail.com>
2020-08-25 15:27:12 +0530

    update

--- src/app/saga01.js
+++ src/app/saga01.js
@@ -1,26 +1,34 @@
 import { call, select, put, take } from "../sagaEffects";
 import { getUserAsync } from "./api";
 import { store, selectUserId } from "../store";
-import { runSaga } from "../reduxSagaImpl";
+import { runSaga, takeEvery } from "../reduxSagaImpl";
 
 const showUserName = (user) => {
   console.log("User:", user.name);
 };
 
-const greet = "baby mama";
-
-function* mySaga(greet) {
-  console.log("Welcome to mySaga", greet);
+function* userSaga() {
   // wait for "getUser" action dispatch to resume saga
   yield take("getUser");
   // read state
   const userId = yield select(selectUserId);
-  // call async/sync functions
+  // call async function
   const user = yield call(getUserAsync, userId);
+  // call sync function
   yield call(showUserName, user);
   // dispatch action
   yield put({ type: "setUserSuccess", payload: user });
+  // fork saga to create several execution contexts
+  // Both saga (the initial one and the new one) will continue to run independently
 }
 
-runSaga(store, mySaga, greet);
+// runs on initial exec by runSaga
+function* mainSaga(greet) {
+  console.log("Welcome to mainSaga", greet);
+  // yield* will not return effect
+  // but run "takeEvery" helper saga
+  yield* takeEvery("getUser", userSaga);
+}
+
+runSaga(store, mainSaga, "baby mama!! xoxo ..");
 store.dispatch({ type: "getUser" });
--- src/reduxSagaImpl.js
+++ src/reduxSagaImpl.js
@@ -1,13 +1,31 @@
+import { fork, take } from "./sagaEffects";
+
+// helper saga
+export function* takeEvery(actionType, saga) {
+  console.log("running takeEvery");
+  // forks a new saga & runs an infinite loop
+  // listening to the action <actionType>
+  yield fork(function* newSaga() {
+    while (true) {
+      const action = yield take(actionType);
+      // yield* will not return effect but run the saga
+      yield* saga(action);
+    }
+  });
+}
+
 export async function runSaga(store, saga, ...args) {
   const waitNextAction = (actionType) =>
     new Promise((resolve) => {
       //"getUser" event listner is registered at the time of running runSaga()
+      // saga registers subscriber for actions to be dispatched
       console.log("Register action listener");
       store.actionsEmitter.once(actionType, resolve);
+      // store.actionsEmitter.on(actionType, resolve);
     });
   try {
     // returns an iterator/generator object
-    // console.log(args);
+    console.log("running mainSaga");
     const it = saga(...args);
     // initiate saga execution
     let result = it.next();
@@ -15,11 +33,11 @@ export async function runSaga(store, saga, ...args) {
       const effect = result.value;
       switch (effect.type) {
         case "fork":
+          console.log("Forking new saga context");
           runSaga(store, effect.saga, ...effect.args);
           result = it.next();
           break;
         case "take":
-          console.log("take effect");
           await waitNextAction(effect.actionType);
           result = it.next();
           break;
